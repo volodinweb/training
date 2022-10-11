1665d2b3960cd4b385cb6a520fedf00d3b2d9d89
Volodin Vladimir <volodinweb@yandex.ru>
2022-10-11 17:03:53 +0300

    add python_acquaintances.py

---
+++ python_acquaintances.py
@@ -0,0 +1,27 @@
+#Задача №1
+length = int(input('Введите длину стороны квадрата: '))
+perimeter = length * 4
+square = length**2
+print(perimeter)
+print(square)
+length1 = int(input('Введите длину стороны квадрата: '))
+vwidth1 = int(input('Введите ширину стороны квадрата: '))
+P = 2 * (length1 + vwidth1)
+S = length1 * vwidth1
+print('Периметр:', P)
+print('Площадь:', S)
+
+#Задача №2
+salary = int(input('Введите заработную плату: '))
+mortgageInterest = int(input('Какой процент от зп уходит на ипотеку: '))
+life = int(input('Какой процент от зп уходит на жизнь: '))
+mortgage = salary / 100 * mortgageInterest * 12
+forlife = salary / 100 * life * 12
+accumulation = salary * 12 - mortgage + forlife
+print('На ипотеку было потрачено: ', (int(mortgage)))
+print('Было накоплено: ', (int(accumulation)))
+
+#Задача №3
+a = '#'
+b = P + S
+print(a * b)
